cc558d273d77a9ad24a6ca114b2d94b4e20d58ab
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-07-25 11:08:13 +0530

    Minor Enhancements

--- scanning.js
+++ scanning.js
@@ -1,20 +1,20 @@
 // JavaScript for handling the "Scan Bottle" button click
-document.getElementById('scan-bottle').addEventListener('click', function() {
+document.getElementById('scan-bottle').addEventListener('click', function () {
     window.location.href = 'scanning.html'; // Make sure the path to scanning.html is correct
 });
 
 // scan.js
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Simulate scanning process
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementById('scan-bottle').textContent = 'Scanning completed! Please return to redeem points.';
-        
+
         // Redirect back to dashboard after 3 seconds
-        setTimeout(function() {
+        setTimeout(function () {
             window.location.href = 'scanning.html';
             // window.location.href = 'ui.html'; // Replace with actual dashboard page URL
         }, 3000);
-    }, 5000); 
-    
-// Simulate 5 seconds of scanning
+    }, 5000);
+
+    // Simulate 5 seconds of scanning
 });
